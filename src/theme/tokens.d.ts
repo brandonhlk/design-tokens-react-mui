20ffd6b00c5a08cb1c70da847ab70b969b040640
Pinardy <pinardy.yang@gmail.com>
2025-09-15 17:31:36 +0800

    initial commit

---
+++ src/theme/tokens.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const PaletteBlue40: string;
+export const PaletteBackgroundPrimary: string;
+export const ButtonContainedPrimaryBgHover: string;
+export const Typography: {
+  fontWeight: string;
+  fontSize: string;
+  fontFamily: string;
+};
+export const ExtendedPalette: {
+  yellow: { 90: string; 100: string };
+  blue: { 20: string; 30: string; 40: string };
+};
+export const ExtendedPalette2Yellow90: string;
+export const ExtendedPalette2Yellow100: string;
+export const ExtendedPalette2Blue20: string;
+export const ExtendedPalette2Blue30: string;
+export const ExtendedPalette2Blue40: string;
+export const Button2ContainedPrimaryBgHover: string;
